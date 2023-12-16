b7d4370960b2baf992fbac757a135372ed59cd3e
WesleyR10 <wesleyribas2015@gmail.com>
2023-12-16 10:52:05 -0300

    Password hash and validation

--- prisma/migrations/20231216025002_create_checkin_and_gym/migration.sql
+++ prisma/migrations/20231216025002_create_checkin_and_gym/migration.sql
@@ -13,6 +13,8 @@ CREATE TABLE "check_ins" (
     "id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "validated_at" TIMESTAMP(3),
+    "user_id" TEXT NOT NULL,
+    "gym_id" TEXT NOT NULL,
 
     CONSTRAINT "check_ins_pkey" PRIMARY KEY ("id")
 );
@@ -28,3 +30,9 @@ CREATE TABLE "gyms" (
 
     CONSTRAINT "gyms_pkey" PRIMARY KEY ("id")
 );
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_gym_id_fkey" FOREIGN KEY ("gym_id") REFERENCES "gyms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,6 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client"; 
+import { appRoutes } from '@/http/routes'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
-
-prisma.user.create({data:{
-  name: 'Diego Fernandes',
-  email: 'diego@email.com',
-  password_hash: '123456'
-}})
+app.register(appRoutes)
