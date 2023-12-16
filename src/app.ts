bf3fefc3df402fb5892a8b8e276c91e733fb9c9d
WesleyR10 <wesleyribas2015@gmail.com>
2023-12-15 22:19:09 -0300

    Integration with prisma orm

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client"; 
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({data:{
+  name: 'Diego Fernandes',
+  email: 'diego@email.com',
+  password_hash: '123456'
+}})
