48460279eec330e365731363bf63675304248951
WesleyR10 <wesleyribas2015@gmail.com>
2023-12-19 17:23:15 -0300

    Implementing application use Cases

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -2,6 +2,7 @@ import { Prisma, User } from '@prisma/client'
 
 // Quais métodos e parâmetros existe no meu repositório.
 export interface UsersRepository { 
+  findById(id: string): Promise<User | null>
   findByEmail(email: string): Promise<User | null>
   create(data: Prisma.UserCreateInput): Promise<User>
 }
