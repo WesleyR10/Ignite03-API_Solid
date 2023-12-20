5e01410706f9a357c67bc3b07015f3916b928e5e
WesleyR10 <wesleyribas2015@gmail.com>
2023-12-20 15:23:38 -0300

    Implementing JWT Fastify and profile controller configuration

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -17,10 +17,16 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({
+    const {user} = await authenticateUseCase.execute({
       email,
       password,
     })
+
+    const token = await reply.jwtSign({}, {
+      sign:{sub: user.id}
+    })
+
+    return reply.status(200).send({token,})
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message })
@@ -28,6 +34,4 @@ export async function authenticate(
 
     throw err
   }
-
-  return reply.status(200).send()
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { authenticate } from '@/http/controllers/authenticate'
 import { register } from './controllers/register'
+import { profile } from './controllers/profile'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  app.get('/me', {onRequest: [verifyJWT]}, profile)
 }
